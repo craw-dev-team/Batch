5fc4cb9dc3f1e5b14f85acbf8d006f95584a37f8
Rohan-craw <rohankumar@craw.in>
2025-04-08 19:09:54 +0530

    Page file

---
+++ frontend/HackersBridge_frontend/src/Pages/BooksHome.jsx
@@ -0,0 +1,15 @@
+import Books from "../Components/dashboard/Books/Books";
+
+
+
+const BooksHome = () => {
+    return (
+        <>
+        <div className={'w-full h-full overflow-hidden pt-1 darkmode'}>
+            <Books />
+        </div>
+        </>
+    )
+}
+
+export default BooksHome;
